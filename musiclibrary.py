10fa65c48852c7b556fa4bf7afae473b35208493
blackheart-35 <parjapatihardik2006@gmail.com>
2025-09-15 10:28:20 +0530

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "mindgames": "https://youtu.be/JImzkv0em6c?si=K0FLKGUSLFipRZpW",
+    "waitforme" : "https://youtu.be/OMmffICtJnQ?si=cYJZT7H4X26aJWdG",
+    "dreamgirl": "https://youtu.be/7pAqWzNIKzE?si=-vhTrxqBaP4toCWi"
+
+}
+
+# you can add link of your Favourite music here to listen them later or listen them by asking jarvis
